29a028864baa1722c4fbd4d6c71ffc43cf301eb2
eltinhozem <eltonmoura2015@gmail.com>
2025-01-14 10:07:58 -0300

    styles components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
+
+import { Providers } from './providers'
+import StyledComponentsRegistry from '@/lib/registry'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
+
+const inter = Inter({ subsets: ['latin'] });
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
@@ -24,10 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
