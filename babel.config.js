f036fa9a5de937b76952e339a9405b8bd1dbc490
eltinhozem <eltonmoura2015@gmail.com>
2025-01-13 23:42:35 -0300

    jest

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Para suporte a JavaScript moderno
+    '@babel/preset-react', // Para JSX e React
+    '@babel/preset-typescript' // Se você estiver usando TypeScript
+  ]
+}
